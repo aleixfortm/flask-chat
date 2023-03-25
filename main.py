169e7edbf522e43f7260bcc64d38a753d708a125
Aleix <aleix.fort45@gmail.com>
2023-03-25 20:06:22 +0100

    Added lobby creation

--- main.py
+++ main.py
@@ -7,8 +7,39 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = 'secret2'
 socketio = SocketIO(app)
 
+rooms = {}
+
+def generate_code(length):
+    while True:
+        code = ''
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            return code
+    
 @app.route("/", methods=["POST", "GET"])
 def home():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        code = request.form.get('code')
+        join = request.form.get('join', False)
+        create = request.form.get('create', False)
+
+        if not name:
+            return render_template("home.html", error='Please enter a valid name')
+        
+        if join != False and not code:
+            return render_template('home.html', error='Please enter a valid room code')
+
+        room = code
+        if create != False:
+            room = generate_code(4)
+            rooms[room] = {'members': 0, 'messages': []}
+            print(rooms)
+        elif room not in rooms:
+            return render_template('home.html', error='Specified room does not exist')
+
     return render_template("home.html")
 
 if __name__ == '__main__':
