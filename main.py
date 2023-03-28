6c499fb437aab8b8cc8417776aea98bca0aad7bf
Aleix <aleix.fort45@gmail.com>
2023-03-28 14:43:45 +0200

    Changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
 
-'poll'
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'secret2'
 socketio = SocketIO(app)
